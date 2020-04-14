4ba6b9165b92389512b8bd16a7849fc0665635e9
Tobin Stultiens <tobin.stultiens@gmail.com>
2020-04-14 16:05:33 +0200

    commit before vuetify install

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,19 @@ import store from './store'
 import * as firebase from 'firebase'
 import "firebase/analytics";
 import "firebase/auth";
+import * as firebaseJson from "./firebase.json";
 
 Vue.config.productionTip = false
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2kutBl0p0OYEnebtackg0biZIUOG-QBg",
-  authDomain: "recipe-manager-e0544.firebaseapp.com",
-  databaseURL: "https://recipe-manager-e0544.firebaseio.com",
-  projectId: "recipe-manager-e0544",
-  storageBucket: "recipe-manager-e0544.appspot.com",
-  messagingSenderId: "66118028606",
-  appId: "1:66118028606:web:1e06d286f56799217b306f",
-  measurementId: "G-VTNKVC8PST"
+  apiKey: firebaseJson.apiKey,
+  authDomain: firebaseJson.authDomain,
+  databaseURL: firebaseJson.databaseURL,
+  projectId: firebaseJson.projectId,
+  storageBucket: firebaseJson.storageBucket,
+  messagingSenderId: firebaseJson.messagingSenderId,
+  appId: firebaseJson.appId,
+  measurementId:firebaseJson.measurementId 
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
