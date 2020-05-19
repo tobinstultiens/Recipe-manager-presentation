725294de4b721aba9c0ee5b5e76083fede15f4f1
Tobin Stultiens <tobin.stultiens@gmail.com>
2020-05-19 23:01:24 +0200

    Updated the recipe service

---
+++ src/models/RecipeList.ts
@@ -0,0 +1,6 @@
+export interface RecipeList{
+	id: string;
+	title: string;
+	description: string;
+	imgLink?: string;
+}
--- src/services/recipes/recipeService.ts
+++ src/services/recipes/recipeService.ts
@@ -1,10 +1,38 @@
 import http from "../httpcommon";
+import { Recipe } from "@/models/Recipe.ts";
+import { RecipeList } from "@/models/RecipeList.ts";
 
-class RecipeService {
-    getAll() {
-        return http.get("/recipe");
-    }
-    get(id: string) {
-        return http.get("/recipe/" + id);
-    }
+export class RecipeService {
+	getAll(): RecipeList[] {
+		http.get("/recipe").then(response => {
+			return response;
+		});
+	}
+	get(id: string): Recipe {
+		http.get("/recipe/" + id).then(response => {
+			return response;
+		});
+	}
+	post(recipe: Recipe): void {
+		http.post("/recipe/", {
+			body: recipe
+		})
+		.then(response => {
+			return response.data;
+		})
+		.catch(error => {
+			console.log(error);
+		});
+	}
+	put(recipe: Recipe): void {
+		http.put("/recipe/", {
+			body: recipe
+		})
+		.then(response => {
+			return response.data;
+		})
+		.catch(error => {
+			console.log(error);
+		});
+	}
 }
