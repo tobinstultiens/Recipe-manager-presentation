0bb9f5fa1e6d0b7e70428b2eb4a02f7453dcea91
Tobin Stultiens <tobin.stultiens@gmail.com>
2020-05-21 23:00:43 +0200

    Created a base layout for the home page of the recipes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <v-btn @click="logout">Logout</v-btn>
-  </div>
+	<div class="home">
+		<RecipeHome />
+		<v-btn @click="logout">Logout</v-btn>
+	</div>
 </template>
 
-<script>
+<script lang="ts">
 import firebase from "firebase";
+import RecipeHome from "@/components/recipe/RecipeHome.vue";
 
 export default {
-  name: "Home",
-  methods: {
-    logout(){
-    firebase.auth().signOut().then(function() {
-  // Sign-out successful.
-}, function() {
-  // An error happened.
-});
-    }
-  }
+	name: "Home",
+	components: {
+		RecipeHome
+	},
+	methods: {
+		logout() {
+			firebase
+				.auth()
+				.signOut()
+				.then(
+					function() {
+						// Sign-out successful.
+					},
+					function() {
+						// An error happened.
+					}
+				);
+		}
+	}
 };
 </script>
